e9a9b409f12b1ef5c3fa92cb3125c4ed1b485a61
Frank Lanitz <frank@frank.uvena.de>
2014-01-18 23:34:11 +0100

    Creating a first, little model for collecting weight data

--- healthfun/weight/admin.py
+++ healthfun/weight/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from weight.models import Weight
+
+admin.site.register(Weight)
--- healthfun/weight/models.py
+++ healthfun/weight/models.py
@@ -1,3 +1,7 @@
+from django.core.urlresolvers import reverse
 from django.db import models
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+class Weight(models.Model):
+    weight = models.FloatField(blank=False, null=False)
+    timestamp = models.DateTimeField()
