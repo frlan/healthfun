2b9ef1fa3b88bae0ab3f92806b9c98bc004e4ddb
Frank Lanitz <frank@frank.uvena.de>
2014-01-18 23:15:19 +0100

    Enabling weight-app

--- healthfun/healthfun/settings.py
+++ healthfun/healthfun/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'south'
+    'south',
+    'weight'
 )
 
 MIDDLEWARE_CLASSES = (
