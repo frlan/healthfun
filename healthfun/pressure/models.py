24b3557f4eb0446322ac5da5083970e146ae7288
Frank Lanitz <frank@frank.uvena.de>
2014-01-19 15:08:51 +0100

    Enabling translations and adding verbose names, marked as translateable

--- healthfun/pressure/models.py
+++ healthfun/pressure/models.py
@@ -1,12 +1,12 @@
 from django.core.urlresolvers import reverse
 from django.db import models
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 class Pressure (models.Model):
-    timestamp = models.DateTimeField()
-    sys = models.FloatField(blank=False, null=False)
-    dia = models.FloatField(blank=False, null=False)
-    pulse = models.IntegerField(blank=False, null=False)
+    timestamp = models.DateTimeField(verbose_name=_(u"Messurement timestamp"))
+    sys = models.FloatField(blank=False, null=False, verbose_name=_(u"systolic"))
+    dia = models.FloatField(blank=False, null=False, verbose_name=_(u"diastolic"))
+    pulse = models.IntegerField(blank=False, null=False, verbose_name=_(u"Pulse"))
     
     def __unicode__(self):
         return self.timestamp.strftime("%y-%m-%d: %H-%M")
--- healthfun/weight/models.py
+++ healthfun/weight/models.py
@@ -1,10 +1,10 @@
 from django.core.urlresolvers import reverse
 from django.db import models
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 class Weight(models.Model):
-    weight = models.FloatField(blank=False, null=False)
-    timestamp = models.DateTimeField()
+    weight = models.FloatField(blank=False, null=False, verbose_name=_(u"Weight"))
+    timestamp = models.DateTimeField(verbose_name=_(u"Messurement timestamp"))
 
     def __unicode__(self):
         return self.timestamp.strftime("%y-%m-%d: %H-%M")
