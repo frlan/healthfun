c22179524f1df8b511d663f85461a5e98d5a90e5
Frank Lanitz <frank@frank.uvena.de>
2014-01-26 19:12:21 +0100

    Adding some very first form to insert data

---
+++ healthfun/messurements/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+
+from .models import Pressure, Weight
+
+class PressureForm(forms.ModelForm):
+
+    class Meta:
+        model = Pressure
+        exclude = ('user', 'timestamp', 'comment' )
+
+class WeightForm(forms.ModelForm):
+
+    class Meta:
+        model = Weight
+        exclude = ('user', 'timestamp', 'comment')
+ 
