a2117a05e45eb5422959f85d1ad0334a82144925
Frank Lanitz <frank@frank.uvena.de>
2014-01-31 18:34:57 +0100

    Some minor cleanups

--- healthfun/healthfun/urls.py
+++ healthfun/healthfun/urls.py
@@ -1,22 +1,16 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.views.generic import TemplateView
 
 # Autentification system
-from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.decorators import login_required
 
 from messurements.views import PressureListView, WeightListView, PressureWeightView
 
 from django.contrib import admin
 admin.autodiscover()
 
-
-
 urlpatterns = i18n_patterns('',
-    # Examples:
-    # url(r'^$', 'healthfun.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^weight/list/$', login_required(WeightListView.as_view()), name="weight-list"),
